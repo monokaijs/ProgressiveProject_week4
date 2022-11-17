73db0e434bc278bc3d184a09a4b39cf9c98e2bb3
Edward Nguyen <monokaijs@gmail.com>
2022-11-17 14:06:03 +0700

    feat image-viewer: add image viewer with pinch gesture

--- app/src/main/java/com/monokaijs/progressive1/MainActivity.java
+++ app/src/main/java/com/monokaijs/progressive1/MainActivity.java
@@ -2,26 +2,43 @@ package com.monokaijs.progressive1;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import com.monokaijs.progressive1.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
   private AppBarConfiguration appBarConfiguration;
   private ActivityMainBinding binding;
+
+  public static List<String> imageList = Arrays.asList(
+      "https://i.imgur.com/FhKzmRn_d.webp?maxwidth=800&fidelity=grand",
+      "https://i.imgur.com/LRoLTlK.jpeg",
+      "https://i.imgur.com/1ofnU.jpeg",
+      "https://i.imgur.com/LIlqion.jpeg",
+      "https://i.imgur.com/9BU396Q.jpeg"
+  );
+  public static int currentIndex = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +53,42 @@ public class MainActivity extends AppCompatActivity {
     appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-    binding.fab.setOnClickListener(new View.OnClickListener() {
+    setImageIndex(0);
+
+    binding.getRoot().findViewById(R.id.btnNext).setOnClickListener(new View.OnClickListener() {
       @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+      public void onClick(View v) {
+        setImageIndex(1);
       }
     });
+
+    binding.getRoot().findViewById(R.id.btnBack).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        setImageIndex(-1);
+      }
+    });
+  }
+
+  private void setImageIndex(int navigateIndex) {
+    ImageView imageView = findViewById(R.id.imageView);
+    currentIndex = currentIndex + navigateIndex;
+
+    if (currentIndex > imageList.size() - 1) {
+      currentIndex = 0;
+    } else if (currentIndex < 0) {
+      currentIndex = imageList.size() - 1;
+    }
+    Log.i("INDEX", String.valueOf(currentIndex));
+
+    CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(this);
+    circularProgressDrawable.setStrokeWidth(5f);
+    circularProgressDrawable.setCenterRadius(30f);
+    circularProgressDrawable.start();
+    Glide.with(this)
+        .load(imageList.get(currentIndex))
+        .placeholder(circularProgressDrawable)
+        .into(imageView);
   }
 
   @Override
