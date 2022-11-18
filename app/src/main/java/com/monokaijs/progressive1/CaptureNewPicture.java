4d81ad73376f8891a297771fb31ee7377af8fa36
Edward Nguyen <monokaijs@gmail.com>
2022-11-19 01:34:20 +0700

    feat camera: add camera ability

---
+++ app/src/main/java/com/monokaijs/progressive1/CaptureNewPicture.java
@@ -0,0 +1,175 @@
+package com.monokaijs.progressive1;
+
+import static android.content.Context.LOCATION_SERVICE;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CaptureNewPicture extends Fragment {
+  static final int REQUEST_IMAGE_CAPTURE = 1;
+  Button btnCapture;
+  Button btnSave;
+  ImageView imgView;
+  String currentFilePath;
+  int LOCATION_REFRESH_TIME = 15000;
+  int LOCATION_REFRESH_DISTANCE = 500;
+  Location currentLocation;
+  LocationManager mLocationManager;
+
+  private final LocationListener mLocationListener = new LocationListener() {
+    @Override
+    public void onLocationChanged(final Location location) {
+      currentLocation = location;
+    }
+  };
+
+  public CaptureNewPicture() {
+    // Required empty public constructor
+  }
+
+  public static CaptureNewPicture newInstance(String param1, String param2) {
+    CaptureNewPicture fragment = new CaptureNewPicture();
+    Bundle args = new Bundle();
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    mLocationManager = (LocationManager) MainActivity.instance.getSystemService(LOCATION_SERVICE);
+
+    if (ActivityCompat.checkSelfPermission(MainActivity.instance, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.instance, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      return;
+    }
+    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+        LOCATION_REFRESH_DISTANCE, mLocationListener);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+
+    View root = inflater.inflate(R.layout.fragment_capture_new_picture, container, false);
+
+    btnCapture = root.findViewById(R.id.btnCapture);
+    btnSave = root.findViewById(R.id.btnSave);
+    imgView = root.findViewById(R.id.imgPreview2);
+
+    btnSave.setEnabled(false);
+
+    btnSave.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Log.i("dsfad", currentLocation.toString());
+      }
+    });
+
+    btnCapture.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        dispatchCamera();
+      }
+    });
+
+    return root;
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    Log.i("RESULT", "ON_RESULT");
+    if (resultCode == Activity.RESULT_OK) {
+      if (requestCode == REQUEST_IMAGE_CAPTURE) {
+        Log.i("RESULT", "CAPTURED");
+        assert data != null;
+        Bitmap bitmapData = (Bitmap) data.getExtras().get("data");
+        imgView.setImageBitmap(bitmapData);
+        btnSave.setEnabled(true);
+        String path = savePhotoToExternalStorage("test", bitmapData);
+        currentFilePath = path;
+        Log.i("OUTPUT", path);
+      }
+    }
+  }
+
+  private String savePhotoToExternalStorage(String name, Bitmap bmp) {
+    File pictureFile = getOutputMediaFile(name);
+    if (pictureFile == null) {
+      Log.d("ERROR", "Error creating media file, check storage permissions: ");// e.getMessage());
+      return "";
+    }
+    try {
+      FileOutputStream fos = new FileOutputStream(pictureFile);
+      bmp.compress(Bitmap.CompressFormat.PNG, 100, fos);
+      fos.close();
+    } catch (FileNotFoundException e) {
+      Log.d("ERROR", "File not found: " + e.getMessage());
+    } catch (IOException e) {
+      Log.d("ERROR", "Error accessing file: " + e.getMessage());
+    }
+
+    return pictureFile.getAbsolutePath();
+  }
+
+  private File getOutputMediaFile(String name){
+    // To be safe, you should check that the SDCard is mounted
+    // using Environment.getExternalStorageState() before doing this.
+    File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+        + "/Android/data/"
+        + MainActivity.instance.getApplicationContext().getPackageName()
+        + "/Files");
+    if (! mediaStorageDir.exists()){
+      if (! mediaStorageDir.mkdirs()){
+        return null;
+      }
+    }
+    File mediaFile;
+    String mImageName="MI_"+ name +".jpg";
+    mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+    return mediaFile;
+  }
+
+  public void dispatchCamera() {
+    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+  }
+}
--- app/src/main/java/com/monokaijs/progressive1/MainActivity.java
+++ app/src/main/java/com/monokaijs/progressive1/MainActivity.java
@@ -1,13 +1,34 @@
 package com.monokaijs.progressive1;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.bumptech.glide.Glide;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -21,19 +42,43 @@ import com.monokaijs.progressive1.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+  public final String APP_TAG = "ProgressiveProject";
+  private static final int MY_PERMISSION_REQUEST_CAMERA = 101;
+  public String photoFileName = "photo.jpg";
+
+  File photoFile;
   public static MainActivity instance;
   private AppBarConfiguration appBarConfiguration;
   private ActivityMainBinding binding;
   public static ImageDataHandler db;
   public static List<StoredImage> imageList = new ArrayList<>();
 
-  // Create list of images
+  AlertDialog alertDialog;
+  String[] permissionsStr = {Manifest.permission.CAMERA,
+      Manifest.permission.RECORD_AUDIO,
+      Manifest.permission.READ_EXTERNAL_STORAGE,
+      Manifest.permission.WRITE_EXTERNAL_STORAGE,
+      Manifest.permission.ACCESS_COARSE_LOCATION,
+      Manifest.permission.ACCESS_FINE_LOCATION
+  };
+  int permissionsCount = 0;
+  ActivityResultLauncher<String[]> permissionsLauncher =
+      registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+          new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String,Boolean> result) {
+
+            }
+          });
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
     appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
   }
 
   @Override
@@ -68,6 +112,13 @@ public class MainActivity extends AppCompatActivity {
     if (id == R.id.action_all_links) {
       getNavController().navigate(R.id.action_ImageViewerFragment_to_imageLinkFragment);
     }
+    if (id == R.id.capture_new) {
+      ArrayList<String> permissionsList = new ArrayList<>(Arrays.asList(permissionsStr));
+      askForPermissions(permissionsList);
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+        getNavController().navigate(R.id.action_ImageViewerFragment_to_captureNewPicture);
+      }
+    }
     return super.onOptionsItemSelected(item);
   }
 
@@ -84,5 +135,32 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
     return NavigationUI.navigateUp(navController, appBarConfiguration)
         || super.onSupportNavigateUp();
+  }
+
+  private void askForPermissions(ArrayList<String> permissionsList) {
+    String[] newPermissionStr = new String[permissionsList.size()];
+    for (int i = 0; i < newPermissionStr.length; i++) {
+      newPermissionStr[i] = permissionsList.get(i);
+    }
+    if (newPermissionStr.length > 0) {
+      permissionsLauncher.launch(newPermissionStr);
+    } else {
+      showPermissionDialog();
+    }
+  }
+
+  private void showPermissionDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle("Permission required")
+        .setMessage("Some permissions are need to be allowed to use this app without any problems.")
+        .setPositiveButton("Settings", (dialog, which) -> {
+          dialog.dismiss();
+        });
+    if (alertDialog == null) {
+      alertDialog = builder.create();
+      if (!alertDialog.isShowing()) {
+        alertDialog.show();
+      }
+    }
   }
 }
